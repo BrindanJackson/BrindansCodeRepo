e1559e5ab59e75d00e2f3e9d8bacdce2860abb8f
BrindanJackson <brindanmj@gmail.com>
2015-03-05 14:40:11 -0500

    Adding Lab2 for Principle of Computer Languages class. Its the start of a redo of lab1 from C++ to Ruby

---
+++ Principles/PrincipleLab2.rb
@@ -0,0 +1,9 @@
+#Tokenizer Class
+class Tokenizer
+  def initialize()
+    @token_list = []
+    @token_types = []
+    @token = ""
+    @unparsed_string = ""
+  end
+end
---
+++ TestCode/TestRuby.rb
@@ -0,0 +1,11 @@
+unparsed_string = ""
+
+index = 0
+
+while unparsed_string.size()
+
+  if unparsed_string[index] == ""
+
+  end
+
+end
