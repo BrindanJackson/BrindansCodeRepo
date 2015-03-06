cdf0e062f926c20c2e7d97f16f3d7b4ad319e9dc
BrindanJackson <brindanmj@gmail.com>
2015-03-06 13:04:23 -0500

    commiting changes

--- Principles/PrincipleLab2.rb
+++ Principles/PrincipleLab2.rb
@@ -5,22 +5,44 @@ class Tokenizer
     @token_types = []
     @token = ""
     @unparsed_string = unparsed
+    @current_index = 0
+    @current_char = ""
   end
 
-	def tokenize
-		index = 0
+  #If not at end of @unparsed_string, increment @current_index
+  #Assign character at @current_index to @current_character
+  def nextChar
+    if !(@unparsed_string.length() == @current_index=1)
+      @current_index = @current_index + 1
+      @current_char = @unparsed_string[@current_index]
+    end
+  #End of function nextChar
+  end
 
-		while index < @unparsed_string.length()-1 do
-			if @unparsed_string[index] == ""
+  def tokenize
+    done = 0
 
-			elsif @unparsed_string[index] == ""
+    while done == 0 do
 
-			elsif @unparsed_string[index] == ""
+      #Goes through character by character to find tokens
 
-			end
-			
-		end
+      #checks lower case reserved words
+      if 
+      
+      #checks Uppercase identifiers
+      elsif
 
-	end
+      #checks integers
+      elsif
 
+      #checks al else
+      elsif
+
+
+      end
+    end
+  #End of function Tokenize
+  end
+
+#end of Class Tokenizer
 end
